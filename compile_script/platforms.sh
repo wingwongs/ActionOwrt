5f91d7c9448be940f2fa429164a0e3def04d6b5b
wingwongs <wingwongs@users.noreply.github.com>
2026-08-17 18:49:10 +0800

    feat: add Qihoo 360V6 (IPQ6000) device for immortalwrt build
    - Added config/immortalwrt_config/360V6.config with qualcommax/ipq60xx target for qihoo_360v6
    - Added 360V6 to immortalwrt_platforms array in platforms.sh

--- compile_script/platforms.sh
+++ compile_script/platforms.sh
@@ -34,7 +34,7 @@ lede_value='{
 }'
 
 openwrt_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S)
-immortalwrt_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S R66S R68S)
+immortalwrt_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S R66S R68S 360V6)
 lede_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S H28K H29K H66K H68K H69K R66S R68S)
 
 matrix_json="["
