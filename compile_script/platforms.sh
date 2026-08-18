12013a40002edab45471d892c70a1db8081d4f96
las <218048983+wingwongs@users.noreply.github.com>
2026-08-18 17:33:58 +0800

    Update platforms.sh

--- compile_script/platforms.sh
+++ compile_script/platforms.sh
@@ -33,8 +33,8 @@ lede_value='{
   "OS": "ubuntu-latest"
 }'
 
-openwrt_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S)
-immortalwrt_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S R66S R68S 360V6)
+openwrt_platforms=(R2S R3S R4S R4SE R5C R5S R6C R6S)
+immortalwrt_platforms=(R2S R3S R4S R4SE R5C R5S R6C R6S R66S R68S 360V6)
 lede_platforms=(X86 R2S R3S R4S R4SE R5C R5S R6C R6S H28K H29K H66K H68K H69K R66S R68S)
 
 matrix_json="["
